5dc085d1b949beb906e9ff608e56bcc10c50beb2
Spyro <spyross.sites@gmail.com>
2023-06-05 06:33:14 -0400

    Created the basic game program that starts the window up

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # Pytris: A practice in Tetris with Pygame
+import sys, pygame
+
+pygame.init()
+
+black = 0,0,0
+width = 800
+height = 800
+size = width, height
+screen = pygame.display.set_mode(size)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            pygame.quit() # Python crashes when trying to close the game if this is not here
+            sys.exit()
+    
+    screen.fill(black)
+    pygame.display.flip()
